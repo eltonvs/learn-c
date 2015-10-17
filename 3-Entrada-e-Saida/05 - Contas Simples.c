41e00a15c611429acf7889aade336885b45e35b4
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2015-10-16 21:16:19 -0300

    Create 05 - Contas Simples.c

---
+++ 3-Entrada-e-Saida/05 - Contas Simples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+  int a, b, soma, sub, mult, divi;
+  printf("Digite dois números: ");
+  scanf("%d %d", %a, %b);
+  soma = a+b;
+  sub = a-b;
+  mult = a*b;
+  divi = a/b;
+  printf("\nO resultado de %d + %d eh %d\n", a, b, soma);
+  printf("O resultado de %d - %d eh %d\n", a, b, sub);
+  printf("O resultado de %d x %d eh %d\n", a, b, mult);
+  printf("O resultado de %d / %d eh %d\n", a, b, divi);
+  return 0;
+}
